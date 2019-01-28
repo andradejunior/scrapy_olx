acc2b2ba657a78effd55e397d0ab136694cffef4
Andrade <andradejr-93@hotmail.com>
2019-01-27 23:13:52 -0300

    Access page of each item.

--- olx/olx/spiders/cars.py
+++ olx/olx/spiders/cars.py
@@ -7,11 +7,19 @@ class CarsSpider(scrapy.Spider):
 
     name = 'cars'
     allowed_domains = ['rn.olx.com.br']
-    start_urls = ['''https://rn.olx.com.br/autos-e-pecas/
-                     carros-vans-e-utilitarios/''']
+    start_urls = ['''https://rn.olx.com.br/autos-e-pecas/'''
+                  '''carros-vans-e-utilitarios/''']
 
     def parse(self, response):
         """Parse method for cars."""
-        items = response.xpath('//ul[@id="main-ad-list"]/li')
+        items = response.xpath(
+            '//ul[@id="main-ad-list"]/li[not(contains(@class, "list_native"))]'
+        )
+        self.log(len(items))
         for item in items:
-            self.log(item.xpath('./a/@href').extract_first())
+            url = item.xpath('./a/@href').extract_first()
+            yield scrapy.Request(url=url, callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        """Parse detail method for cars."""
+        self.log(response.url)
