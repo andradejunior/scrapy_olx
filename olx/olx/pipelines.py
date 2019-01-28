cca939f2de87ccfbde584236e2ef8d76a7656ab3
Andrade <andradejr-93@hotmail.com>
2019-01-28 00:26:55 -0300

    Item pipeline with mongodb.

--- olx/olx/pipelines.py
+++ olx/olx/pipelines.py
@@ -1,11 +1,35 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+"""Item pipeline."""
+import pymongo
 
 
-class OlxPipeline(object):
+class MongoPipeline(object):
+    """Pipeline class for mongodb."""
+
+    collection_name = 'cars'
+
+    def __init__(self, mongo_uri, mongo_db):
+        """Init method."""
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        """From crawler class method."""
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        """Method that execute when open spider."""
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        """Method that xecute when close spider."""
+        self.client.close()
+
     def process_item(self, item, spider):
+        """Process item method."""
+        self.db[self.collection_name].insert_one(dict(item))
         return item
